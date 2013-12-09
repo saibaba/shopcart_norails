ba9499dae6bb3c923a2b8ef8288782c2f5c3acd0
Saibaba Telukunta <saibaba.telukunta@gmail.com>
2013-12-09 08:21:13 -0600

    destroy validation for Product; enhanced app

--- app.rb
+++ app.rb
@@ -4,19 +4,48 @@ dbconfig = YAML::load(File.open('database.yml'))
 ActiveRecord::Base.establish_connection(dbconfig)
 
 def createSampleProducts
-  p  = Product.new
-  p.title = "ipad"
-  p.save
+
+    o = Product.find_by(title: "ipad")
+    if o == nil
+        puts "Products do not exist, creating..."
+        p  = Product.new
+        p.title = "ipad"
+        p.save
+        p p.errors
+    else
+        puts "Products exist, re-using..."
+        puts o
+    end
+
 end
 
 def sampleCart
   c = Cart.new
   l = LineItem.new
-  l.product = Product.find(1)
+  l.product = Product.find_by(title: "ipad")
   c.line_items << l
   c.save
+  p c.errors
 end
 
-#LineItem.new.save
-#createSampleProducts
+def destroyProduct
+    puts "Creating  and destroying product"
+    p = Product.find_by(title: "ipad")
+    p.destroy
+    p p.errors
+end
+
+def createAndDestroyProduct
+    p = Product.new
+    p.title = "delete me"
+    p.save
+    p p.errors
+    p.destroy
+    p p.errors
+end
+
+createSampleProducts
 sampleCart
+destroyProduct
+createAndDestroyProduct
+
